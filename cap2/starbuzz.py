4e1156c85ade729c5b04483763363a26a124ce8f
Thiago Silveira <62719017+silveiratcl@users.noreply.github.com>
2022-08-18 09:39:44 -0300

    chapter 2 done

--- cap2/starbuzz.py
+++ cap2/starbuzz.py
@@ -34,3 +34,46 @@ end_price = start_price + 4
 price = text[start_price:end_price]
 
 print(price)
+
+
+# challenge #4
+# The price position varies along the html code
+# the challenge is to "find" some charaters to track the price
+# print if the price while $ > 4.74
+
+import urllib.request # library
+price = 99.99
+while price > 4.74:
+
+    page = urllib.request.urlopen("http://beans.itcarlow.ie/prices-loyalty.html") # library.function()
+    text = page.read().decode("utf8")
+    where = text.find(">$")
+
+    start_price = where + 2
+    end_price = start_price + 4
+
+    price = float(text[start_price:end_price])
+
+print("Buy!")
+
+
+
+
+
+# challenge #5
+# limit the number of requests by time using the library "time"
+
+import urllib.request # librar yurllib.request
+import time
+price = 99.99
+while price > 4.74:
+
+    time.sleep(900)
+    page = urllib.request.urlopen("http://beans.itcarlow.ie/prices-loyalty.html") # library.function()
+    text = page.read().decode("utf8")
+    where = text.find(">$")
+    start_price = where + 2
+    end_price = start_price + 4
+    price = float(text[start_price:end_price])
+
+print("Buy!", time_read)
